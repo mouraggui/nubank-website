b84bba8966031863178e712ed379a73a18783a84
Guilherme Moura <mouraggui@gmail.com>
2020-04-15 09:54:34 -0300

    Updates

---
+++ precache-manifest.2c837e954cbe40423b4e66ab4b9faec0.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f8276e34ecb8a029710c9621f5c776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a2e67a2ea134e8e32e",
+    "url": "/static/css/main.268035d1.chunk.css"
+  },
+  {
+    "revision": "f615f690f090d9eefda4",
+    "url": "/static/js/2.5d88629a.chunk.js"
+  },
+  {
+    "revision": "67a2e67a2ea134e8e32e",
+    "url": "/static/js/main.09ecb0c7.chunk.js"
+  },
+  {
+    "revision": "e482fca20412e3f9033d",
+    "url": "/static/js/runtime~main.1971d53b.js"
+  },
+  {
+    "revision": "ea8cbdeb0c48c1f9fdd6b8945a7c9909",
+    "url": "/static/media/HankenGrotesk-Black.ea8cbdeb.woff"
+  },
+  {
+    "revision": "37ad59a5e2dcd8b456d26792e4da0c9b",
+    "url": "/static/media/HankenGrotesk-Bold.37ad59a5.woff"
+  },
+  {
+    "revision": "df952af31b8feeb93fc1270ee6d9e381",
+    "url": "/static/media/HankenGrotesk-ExtraLight.df952af3.woff"
+  },
+  {
+    "revision": "5c3ba2e72a290caa943d71067fd80900",
+    "url": "/static/media/HankenGrotesk-Light.5c3ba2e7.woff"
+  },
+  {
+    "revision": "7ed163a63c0d38f106433ec9059eb947",
+    "url": "/static/media/HankenGrotesk-Medium.7ed163a6.woff"
+  },
+  {
+    "revision": "a3d1b1bb79e0160c52337a5c9bc62058",
+    "url": "/static/media/HankenGrotesk-Regular.a3d1b1bb.woff"
+  },
+  {
+    "revision": "fcd88c0bc895e9cff129f257861a4e9b",
+    "url": "/static/media/HankenGrotesk-SemiBold.fcd88c0b.woff"
+  },
+  {
+    "revision": "9e4810a64ff59eda3cf02d3e2721b3d2",
+    "url": "/static/media/bars.9e4810a6.svg"
+  },
+  {
+    "revision": "fba332970a63383c7e2235f4451fb34e",
+    "url": "/static/media/challenge-system-large.fba33297.jpg"
+  },
+  {
+    "revision": "286fdd0ff6e50a0545e41aebc28c9932",
+    "url": "/static/media/holding-card-vertical-0.5x.286fdd0f.png"
+  },
+  {
+    "revision": "286fdd0ff6e50a0545e41aebc28c9932",
+    "url": "/static/media/holding-card-vertical-0.75x.286fdd0f.png"
+  },
+  {
+    "revision": "67f56f77bceb87b365d14699e24797a6",
+    "url": "/static/media/holding-card-vertical-1x.67f56f77.png"
+  },
+  {
+    "revision": "53e1bec61b643a8d2eb46d7227ad493e",
+    "url": "/static/media/holding-card-vertical-2x.53e1bec6.png"
+  },
+  {
+    "revision": "2c8350fc20900c8701c295deaa9d7725",
+    "url": "/static/media/holding-card-vertical-3x.2c8350fc.png"
+  },
+  {
+    "revision": "d13d970247e6e584fabe084714735a1f",
+    "url": "/static/media/holding-device-smile-0.5x.d13d9702.jpg"
+  },
+  {
+    "revision": "c5dbef0a306f6bdf03b120b09ace5721",
+    "url": "/static/media/holding-device-smile-3x.c5dbef0a.jpg"
+  },
+  {
+    "revision": "52250c11a23e1c09422b3c639b624557",
+    "url": "/static/media/home_products-0.5x.52250c11.png"
+  },
+  {
+    "revision": "7ee5eb51512856ffa8db59bbbb621036",
+    "url": "/static/media/home_products-0.75x.7ee5eb51.png"
+  },
+  {
+    "revision": "5220afeb6ae4bd0c7117d872c7043c98",
+    "url": "/static/media/home_products-1x.5220afeb.png"
+  },
+  {
+    "revision": "21ff7f58547a92f305d6905292bf6f6e",
+    "url": "/static/media/home_products-3x.21ff7f58.png"
+  },
+  {
+    "revision": "0175300e4bd54af0ba10e53205439bbe",
+    "url": "/static/media/nubank.0175300e.svg"
+  },
+  {
+    "revision": "4d9ad9a4b0cb984103883a5827f369f1",
+    "url": "/static/media/nucommunity-0.5x.4d9ad9a4.jpg"
+  },
+  {
+    "revision": "80f486c2b91c20e47e2631dfc00304aa",
+    "url": "/static/media/nucommunity-3x.80f486c2.jpg"
+  },
+  {
+    "revision": "e507883743b04ac6d14165baa96d1e01",
+    "url": "/static/media/nuconta-inclined-device-0.5x.e5078837.png"
+  },
+  {
+    "revision": "e507883743b04ac6d14165baa96d1e01",
+    "url": "/static/media/nuconta-inclined-device-0.75x.e5078837.png"
+  },
+  {
+    "revision": "2093b58be59ab2537cf393cbc33ad2f0",
+    "url": "/static/media/nuconta-inclined-device-1x.2093b58b.png"
+  },
+  {
+    "revision": "046430cdb02a492cf784541d83b4de14",
+    "url": "/static/media/nuconta-inclined-device-2x.046430cd.png"
+  },
+  {
+    "revision": "e821a6564497a8a5a1085748bff593d2",
+    "url": "/static/media/nuconta-inclined-device-3x.e821a656.png"
+  },
+  {
+    "revision": "18076de4ec51b69f4abfcf11353d4f57",
+    "url": "/static/media/rewards-home-card-0.5x.18076de4.png"
+  },
+  {
+    "revision": "b5ec850f111cc5b4682e5b9ade908e8d",
+    "url": "/static/media/rewards-home-card-0.75x.b5ec850f.png"
+  },
+  {
+    "revision": "09cdb2f9d9f1fc90aff34de3ccfc8cfe",
+    "url": "/static/media/rewards-home-card-1x.09cdb2f9.png"
+  },
+  {
+    "revision": "7ae982060f135776fbbca561c68cce44",
+    "url": "/static/media/rewards-home-card-2x.7ae98206.png"
+  },
+  {
+    "revision": "772f0b3038ba61c6198d6c9d50b3e190",
+    "url": "/static/media/rewards-home-card-3x.772f0b30.png"
+  },
+  {
+    "revision": "cea71ac2469039e15458b8642952fa16",
+    "url": "/static/media/woman-jumping-0.5x.cea71ac2.jpg"
+  },
+  {
+    "revision": "ef9337bd1ecd526dd0999b5dbe706bd2",
+    "url": "/static/media/woman-jumping-0.75x.ef9337bd.jpg"
+  },
+  {
+    "revision": "1bd12ed4c1e72ed0035577219ca21042",
+    "url": "/static/media/woman-jumping-1x.1bd12ed4.jpg"
+  },
+  {
+    "revision": "f24f4566db1dece30d1f92d17ca45e26",
+    "url": "/static/media/woman-jumping-2x.f24f4566.jpg"
+  },
+  {
+    "revision": "023d9b81c6f95d47a8f0cc47d302fea5",
+    "url": "/static/media/woman-jumping-3x.023d9b81.jpg"
+  }
+]);
